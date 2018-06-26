a1b35fecab5d45032822e4ef12a4161dbd98428e
LaeLaeWin <emolay491997@gmail.com>
2018-06-25 22:22:58 -0700

    Change ex10.py of python

--- ex10.py
+++ ex10.py
@@ -1,9 +1,9 @@
 tabby_cat = "\tI'm tabbed in."
 persian_cat = "I'm split\non a line." 
-backslash_cat = "I'm \\ a \\ cat." 
+backslash_cat = "I'm \\ a \\ dog." 
 fat_cat = """ 
 I'll do a list: 
-\t* Cat food 
+\t* dog food 
 \t* Fishies 
 \t* Catnip\n\t* Grass 
 """ 
